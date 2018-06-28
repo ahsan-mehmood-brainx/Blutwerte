62a0c8adff1841c556f7c2d28b4a368886f43ed9
Hunain Shahid <link2hunain@gmail.com>
2018-06-28 16:23:41 +0500

    Added AppConstant in Utils

---
+++ BaseComponents/Utils/AppConstant.swift
@@ -0,0 +1,36 @@
+//
+//  AppConstant.swift
+//  BaseComponents
+//
+//  Created by Hunain Shahid on 28/06/2018.
+//  Copyright © 2018 Brainx Technologies. All rights reserved.
+//
+
+import Foundation
+
+class AppConstant {
+    struct Title {
+        static let popupDelete = "Delete"
+    }
+    
+    struct Message {
+        static let popupDelete = "Are you sure you want to delete?"
+    }
+    
+    struct StoryboardId {
+        static let main = "Main"
+    }
+    
+    struct ControllerId {
+        static let mainViewController = "mainViewController"
+    }
+    
+    struct Server {
+        static let BASE_URL = "https://www.google.com"
+        static let LOGIN_ENDPOINT = BASE_URL + "/login"
+    }
+    
+    struct Segue {
+        static let TO_LOGIN_CONTROLLER = "loginController"
+    }
+}
