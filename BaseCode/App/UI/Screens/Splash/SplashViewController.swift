1c882fe7b1c0ba6b827683715be61d16f2000f1d
ahsan-mehmood-brainx <ahsan.mehmood@brainxtech.com>
2023-08-23 16:42:34 +0500

    Animation Done

--- BaseCode/App/UI/Screens/Splash/SplashViewController.swift
+++ BaseCode/App/UI/Screens/Splash/SplashViewController.swift
@@ -8,5 +8,37 @@
 import UIKit
 
 class SplashViewController: ViewController<SplashViewModel> {
-
+    
+    //MARK: - Outlets
+    
+    @IBOutlet var splashView: SplashView!
+    
+    //MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialSetup()
+    }
+    
+    //MARK: - Action Methods
+    
+    @objc
+    private func moveToNextScreen() {
+        print("d:D")
+    }
+    
+    //MARK: - Private Methods
+    
+    private func initialSetup() {
+        splashView.splashAnimation.contentMode = .scaleAspectFit
+        splashView.splashAnimation.play()
+        Timer.scheduledTimer(
+            timeInterval: 3.6,
+            target: self,
+            selector: #selector(moveToNextScreen),
+            userInfo: nil,
+            repeats: false
+        )
+        print("d:",splashView.splashAnimation.)
+    }
 }
